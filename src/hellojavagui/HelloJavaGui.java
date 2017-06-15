91559c8fdf344135ec6fe6113e99854bf8cdad50
regymahmudazis@gmail.com <>
2017-06-15 20:15:05 +0700

    Perubahan Pertama .ignore Menggunakan NetBeans

---
+++ src/hellojavagui/HelloJavaGui.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hellojavagui;
+
+/**
+ *
+ * @author g
+ */
+public class HelloJavaGui {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        FrmHello hello = new FrmHello();
+        hello.setVisible(true);
+    }
+    
+}
